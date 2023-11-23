8425c711da1c49739e5d729bc67b860203a68fad
Jackson Woodruff <woodruff.jackson@gmail.com>
2023-11-23 01:13:08 -0500

    Fix model

--- model.py
+++ model.py
@@ -8,34 +8,46 @@ import torchvision.transforms as transforms
 
 import math
 
+BATCH_SIZE = 64
 
 class CustomLinear(nn.Module):
     def __init__(self, input_features, output_features):
         super(CustomLinear, self).__init__()
         # Initialize weights and bias
-        self.weights = nn.Parameter(torch.randn(output_features, input_features))
         self.bias = nn.Parameter(torch.randn(output_features))
 
-        self.input_features = input_features
+        # div features by 2 internall to account for difference
+        # between real and imaginary
+        self.input_features = input_features // 2
         self.pad_to = 2 ** (math.ceil(math.log2(max(input_features, output_features))))
-        self.output_features = output_features
+        self.output_features = output_features // 2
+
+        self.weights = nn.Parameter(torch.randn(self.pad_to))
 
     def forward(self, x):
         # Perform the linear operation: y = xW^T + b
-        if self.input_features < self.output_features:
-            x = torch.nn.functional.pad(x, (0, self.output_features - self.input_features))
-        x_fft = fft(x, self.weights)
+        if self.input_features < self.pad_to:
+            x = torch.nn.functional.pad(x, (0, self.pad_to - self.input_features))
 
-        if self.output_features < self.input_features:
-            x_fft = x_fft[0: output_features]
+        results = []
+        for i in range(x.size(0)):
+            x_sub = x[i]
+            x_fft = fft(x_sub, self.weights)
+            results.append(x_fft)
+
+        x_fft = torch.stack(results)
+        if self.output_features < self.pad_to:
+            x_fft = x_fft[0: self.output_features]
 
         return x_fft
 
 def to_real(imag):
-    return torch.view_as_real(imag).view(-1, 1)
+    num = imag.shape[-2]
+    return torch.view_as_real(imag).view(BATCH_SIZE, num * 2, 1)
 
 def to_imag(real):
-    return torch.view_as_complex(real.view(-1, 2))
+    last = real.shape[-1]
+    return torch.view_as_complex(real.view(BATCH_SIZE, last // 2, 2))
 
 
 def fft(x, twiddle_factors):
@@ -67,14 +79,14 @@ class SimpleMLP(nn.Module):
 
     def forward(self, x):
         x = self.hidden(to_imag(x))
-        x = self.relu(to_real(x))
+        x = torch.squeeze(self.relu(to_real(x)))
         x = self.output(to_imag(x))
         return to_real(x)
 
 # Load MNIST dataset
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 
 # Model, Loss, and Optimizer
 input_size = 784  # 28x28 pixels flattened
